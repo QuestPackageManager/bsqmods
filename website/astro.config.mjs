48ff50a7e3a5315e005bf7c48a88068a217945b0
Daniel <790119+DanTheMan827@users.noreply.github.com>
2024-07-03 20:52:59 +0000

    Replace fontawesome with heroicons

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import playformCompress from "@playform/compress";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   outDir: './out',
-  integrations: [tailwind(), playformCompress()]
+  integrations: [react(), tailwind(), playformCompress()]
 });
