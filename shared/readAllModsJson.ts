0000ff222f4a417ad911a1ceb2f0b9cb005227ca
Daniel <790119+DanTheMan827@users.noreply.github.com>
2024-07-12 04:22:46 +0000

    Fix "global" already defined issue with Astro
    Importing JSON with a "global" key seems to cause issues...

---
+++ shared/readAllModsJson.ts
@@ -0,0 +1,7 @@
+import { ModsCollection } from "./types/ModsCollection";
+import { allModsPath } from "../scripts/shared/paths"
+import { readFileSync } from "fs";
+
+export function readAllModsJson(): ModsCollection {
+  return JSON.parse(readFileSync(allModsPath, "utf-8"));
+}
