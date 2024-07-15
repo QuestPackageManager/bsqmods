c34ed047fa959902f4a74ad25f72d10ba88349b9
Daniel <790119+DanTheMan827@users.noreply.github.com>
2024-07-15 05:44:46 +0000

    Add cleanup missing mods workflow

--- scripts/delete-missing-mods.ts
+++ scripts/delete-missing-mods.ts
@@ -1,11 +1,14 @@
 import { unlinkSync } from "fs";
 import { iterateSplitMods } from "./shared/iterateMods";
+import { fetchHead } from "../shared/fetch";
 
 for (const iteration of iterateSplitMods()) {
+  console.log(iteration.shortModPath);
+
   try {
     const json = iteration.getModJson();
 
-    if ((/^https?:\/\/cdn\.discordapp\.com/).exec(json?.download || "")) {
+    if (!(await fetchHead(json.download))) {
       unlinkSync(iteration.modPath)
     }
   } catch (err) {
