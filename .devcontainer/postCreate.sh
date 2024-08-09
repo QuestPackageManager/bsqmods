e884be4ecd045be10072c68b95639ef6682cf192
Daniel <790119+DanTheMan827@users.noreply.github.com>
2024-08-09 15:38:40 +0000

    Fix postCreate.sh
    Cache was being downloaded at the wrong time.

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,8 +3,9 @@ cd "$(readlink -f "$(dirname "$0")/..")"
 
 (cd "scripts" && npm i)
 (cd "website" && npm i)
-(cd "scripts" && npx tsx combine.ts)
 
 if [[ -n $GITHUB_REPOSITORY ]]; then
   wget --quiet "https://github.com/$GITHUB_REPOSITORY/releases/download/cache/cache.txz" -O - | tar -xJvf -
 fi
+
+(cd "scripts" && npx tsx combine.ts)
